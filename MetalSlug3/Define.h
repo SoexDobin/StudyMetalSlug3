0b2923d160621b34da7d11218b5ca440c69ad695
SoExDobin <56966606+SoexDobin@users.noreply.github.com>
2025-09-16 19:00:34 +0900

    chore : init

--- MetalSlug3/Define.h
+++ MetalSlug3/Define.h
@@ -1,10 +1,21 @@
 #pragma once // 삭제하지 맙시다
 
 // Mecro
-#define			WINCX		640
-#define			WINCY		480
+#define			WINCX		960
+#define			WINCY		720
+
+#define			PURE		= 0
+#define			VK_MAX		0xff
 
 // World Variable
 extern HWND			g_hWnd;
 
 // Enum
+enum ARMORTYPE
+{
+	BIO,
+	MACHINE,
+	ARMORTYPE_END
+};
+
+
